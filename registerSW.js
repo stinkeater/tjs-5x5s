0dbb75fbe7220bb9cf0e88ae9e101f6219064b68
tjborg <tjborg2@gmail.com>
2025-10-14 13:58:10 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tjs-5x5s/sw.js', { scope: '/tjs-5x5s/' })})}
